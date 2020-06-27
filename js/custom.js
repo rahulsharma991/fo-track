5e98bf0ce9750faba8f338a1e11f2bb2f425edc6
Rahul Sharma <rahulsharma94595@gmail.com>
2020-06-27 22:14:35 +0530

    (Initial Push)

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+ function collapse() {
+     
+    var expand = document.getElementsByClassName('enlarge')[0];
+     if(expand.style.display == 'block') {
+         expand.style.display = 'none';
+     } else {
+          expand.style.display = 'block';
+     }
+ }
